3683e9df3976f5d630ee1a8906970a4088846ef8
Kacper Kalinowski <kkacper99@o2.pl>
2023-12-15 16:37:59 +0100

    added menu-handlebar template and template json

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
+const hbs = require('handlebars');
+const path = require('path');
+const data = require('./database-template.json');
+
+
+const compile = async function(templateName, data){
+    const filePath = path.join(process.cwd(), 'templates',`${templateName}.hbs`);
+    const html = await fs.readFile(filePath, 'utf-8');
+    return hbs.compile(html)(data);
+};
+
+
 
 (async function(){
     try{
@@ -7,10 +19,12 @@ const fs = require('fs-extra');
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        await page.setContent('<h1> hello </h1>');
+        const content = await compile('menu-base',data);
+
+        await page.setContent(content);
         await page.emulateMediaType('screen');
         await page.pdf({
-            path: 'mypdf.pdf',
+            path: 'menu.pdf',
             format: 'A4',
             printBackground: true
         });

d5f919485d7174af1ef8cf1f214a8d5c78ef969b
rtrzmielewski <eryoop@gmail.com>
2024-01-04 00:54:47 +0100

    added cors for middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
 const jsonRoute = require('./jsonRoute');
 
